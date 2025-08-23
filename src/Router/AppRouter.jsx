b79efd1d1c52ad6eee96bb4913bd6b56138636c5
Sachin Raj <sachinraj6392@gmail.com>
2025-08-23 22:12:35 +0530

    initial commit

---
+++ src/Router/AppRouter.jsx
@@ -0,0 +1,48 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../components/Footer";
+
+import Home from "../Pages/Home";
+import Menu from "../Pages/Menu";
+import Booking from "../Pages/Booking";
+import Contact from "../Pages/Contact";
+import About from "../Pages/About";
+import Admin from "../AdminPages/Admin";
+import Login from "../Authentication/Login";
+import Register from "../Authentication/Register";
+import ChatApp from "../Chating/chatApp";
+
+
+// Helper wrapper to use useLocation correctly
+function AppContent() {
+    const location = useLocation();
+    const isAdminRoute = location.pathname === '/admin';
+
+    return (
+        <div className="flex flex-col min-h-screen">
+            {!isAdminRoute && <Navbar />}
+
+            <div className="fix bl-0">
+                <ChatApp />
+            </div>
+
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/booking" element={<Booking />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+
+export default AppContent;
