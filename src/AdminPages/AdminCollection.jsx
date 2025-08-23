85370a426320cc67b1de086ff2f85c3b3865e7a5
Sachin Raj <sachinraj6392@gmail.com>
2025-08-24 01:42:28 +0530

    add all component

---
+++ src/AdminPages/AdminCollection.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./AdminNavbar/Navbar";
+import Dashboard from "./Dashboard/Dashboard";
+import Orders from "./AdminPages/Orders";
+import Reservations from "./AdminPages/Reservations";
+import Menu from "./AdminPages/Menu";
+import Gallery from "./Gallery/Gallery";
+import Settings from "./Settings/Settings";
+
+function AdminCollection() {
+    return (
+        <div className="flex">
+            <Navbar />
+            <main className="flex-1 p-6 bg-gray-50 min-h-screen">
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/reservations" element={<Reservations />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/settings" element={<Settings />} />
+                </Routes>
+            </main>
+        </div>
+    );
+}
+
+export default AdminCollection;
