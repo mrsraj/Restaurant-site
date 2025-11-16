03432deab45dded3e749c205b409df53d030bfb2
Sachin Raj <sachinraj6392@gmail.com>
2025-11-17 01:40:07 +0530

    modify

--- src/ProtectedRoute/ProtectedRoute.jsx
+++ src/ProtectedRoute/ProtectedRoute.jsx
@@ -2,13 +2,12 @@ import { Navigate } from "react-router-dom";
 import { useMyContext } from "../context/AppContext";
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-    const { user } = useMyContext();
-
-    console.log("User = ", user);
+    const user = localStorage.getItem('user');
+    console.log("user = ", user);
     
     if (!user) return <Navigate to="/login" />;
 
-    if (!allowedRoles.includes(user.role)) return <Navigate to="/" />;
+    if (!allowedRoles.includes(user)) return <Navigate to="/" />;
 
     return children;
 };
