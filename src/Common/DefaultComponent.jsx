b64f46140ca350c8f043e8446dba1b416ba3d509
Sachin Raj <sachinraj6392@gmail.com>
2025-11-17 01:35:55 +0530

    add default path

--- src/Common/DefaultComponent.jsx
+++ src/Common/DefaultComponent.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800 text-center px-4">
-            <h1 className="text-7xl font-bold mb-2">404</h1>
-            <p className="text-lg mb-6">Oops! The page you're looking for doesn't exist.</p>
-            <Link
-                to="/"
-                className="px-5 py-2 border border-blue-500 text-blue-500 rounded hover:bg-blue-500 hover:text-white transition duration-200"
-            >
-                Go back home
-            </Link>
-        </div>
-    );
+
+    const navigate = useNavigate();
+
+    // Redirect to home after some action (or immediately)
+    React.useEffect(() => {
+        navigate("/admin/dashboard", { replace: true });
+    }, []);
+
 };
 
 export default NotFound;
+
