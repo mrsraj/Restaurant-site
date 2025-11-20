47a4109c86351e2c8c02ec7180d7ae65d8377308
Sachin Raj <sachinraj6392@gmail.com>
2025-11-21 00:05:58 +0530

    modify

--- src/Common/DefaultComponent.jsx
+++ src/Common/DefaultComponent.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import { useMyContext } from '../context/AppContext';
 
 const NotFound = () => {
 
     const navigate = useNavigate();
+    const { user } = useMyContext();
 
     // Redirect to home after some action (or immediately)
     React.useEffect(() => {
-        navigate("/admin/dashboard", { replace: true });
+        if (user.role === "admin") {
+            navigate("/admin/dashboard", { replace: true });
+        }
+        else {
+            navigate("/", { replace: true });
+        }
     }, []);
 
 };
--- src/Common/DishCard.jsx
+++ src/Common/DishCard.jsx
@@ -1,25 +1,64 @@
 import { FiShoppingCart } from "react-icons/fi";
 
-
 export default function DishCard({ dish }) {
 
     function handleCart() {
-        console.log("Add cart successfully!");
-        alert("Add cart successfully!");
+        console.log("Added to cart successfully!");
+        alert("Added to cart successfully!");
     }
- 
+
+    // calculate discounted price
+    const finalPrice = dish.discount
+        ? (dish.price - dish.discount).toFixed(2)
+        : dish.price;
+
     return (
-        <div className="border rounded-lg overflow-hidden shadow hover:shadow-lg transition p-4">
-            <img src={dish.image} alt={dish.name} className="w-full h-48 object-cover" />
+        <div className="border rounded-lg overflow-hidden shadow hover:shadow-lg transition p-4 bg-[#fff] cursor-pointer">
+
+            {/* Dish Image */}
+            <img
+                src={'./images/biryani.jpg'}
+                alt={dish.name}
+                className="w-full h-60 object-cover"
+            />
+
             <div className="p-4 text-center">
-                <h3 className="font-bold text-lg">{dish.name}</h3>
-                <p className="text-red-600 font-semibold">${dish.price}</p>
+
+                {/* Dish Name */}
+                <h3 className="font-bold text-xl">{dish.name}</h3>
+
+                {/* Category Name */}
+                {/* <p className="text-sm text-gray-500">{dish.c_name}</p> */}
+
+                {/* Description */}
+                <p className="text-gray-600 mt-1">{dish.descriptions}</p>
+
+                {/* Price & Discount */}
+                <div className="mt-2">
+                    {dish.discount > 0 ? (
+                        <>
+                            <p className="text-[#07e8f0] font-semibold text-lg">₹{finalPrice}{" "}
+                                <span className="line-through text-gray-500 text-sm">₹{dish.price}</span>
+                            </p>
+                            
+                        </>
+                    ) : (
+                        <p className="text-[#07e8f0] font-semibold text-lg">₹{dish.price}</p>
+                    )}
+                </div>
+
             </div>
-            <div onClick={handleCart}
-                className="flex justify-center items-center bg-[#63b36a] p-2 rounded-md cursor-pointer">
-                <FiShoppingCart size={25} className="pr-2" />
+
+            {/* Add to Cart Button */}
+            <div
+                onClick={handleCart}
+                className="flex justify-center items-center bg-[#63b36a] 
+                text-white p-2 rounded-md cursor-pointer hover:bg-green-600 transition relative bottom- -4"
+            >
+                <FiShoppingCart size={22} className="mr-2" />
                 Add to Cart
             </div>
+
         </div>
     );
 }
