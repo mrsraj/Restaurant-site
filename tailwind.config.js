d4a2b1f4186f853ecbac1be1754c1e4b02a2b140
Sachin Raj <sachinraj6392@gmail.com>
2025-11-23 19:55:30 +0530

    install toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loader: {
+          "0%": { transform: "translateX(-100%)" },
+          "50%": { transform: "translateX(20%)" },
+          "100%": { transform: "translateX(120%)" },
+        },
+      },
+      animation: {
+        loader: "loader 1.4s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 }
