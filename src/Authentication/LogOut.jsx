61bc1f49bedf9e39d43d21c27e70881b30839851
Sachin Raj <sachinraj6392@gmail.com>
2025-12-04 22:55:58 +0530

    inclide navigation

--- src/Authentication/LogOut.jsx
+++ src/Authentication/LogOut.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useMyContext } from "../context/AppContext";
 
 export const useLogout = () => {
     const { setAuth } = useMyContext();
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     const logout = () => {
         console.log("Logout Successfully");
         localStorage.clear();
-        setMenuOpen(!menuOpen);
+        setMenuOpen(false); // reset menu state
         setAuth('');
+        navigate('/home', { replace: true }); // redirect to home
     };
 
     return { logout, menuOpen, setMenuOpen };
