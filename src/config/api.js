4f24720ad0c92d8eefe5f99464eedaf12e3cc672
Sachin Raj <sachinraj6392@gmail.com>
2026-09-13 22:00:59 +0530

    update API_BASE_URL to use localhost for local development

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,3 @@
 // src/config/api.js
-export const API_BASE_URL = "https://ternarytech.online";
+//export const API_BASE_URL = "https://ternarytech.online";
+export const API_BASE_URL = "http://localhost:3000";
